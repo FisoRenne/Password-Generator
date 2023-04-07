ce93e5467ff14de07163482f0579cdbe1d477d19
Sophia Perez <sophiaperez@MacBook-Air.attlocal.net>
2023-04-07 18:44:55 -0500

    added script

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-var generateBtn = document.querySelector("#generate");
-var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var password=document.getElementById("password");
-var minPasswordLength = 8;
-var maxPasswordLength = 128;
-var password = "";
-function generatePassword () {
-  for (var i = 0; i >= minPasswordLength, i <=maxPasswordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * characters.length);
-    password += characters.substring(randomNumber, randomNumber +1);
-  }
 
-}
- {
-        document.getElementById("password").value = password;
+ var generateBtn = document.querySelector("#generate");
+ var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var password=document.getElementById("password");
+ var minPasswordLength = 8;
+ var maxPasswordLength = 128;
+
+ function generatePassword() {
+     var password = "";
+   for (var i = 0; i >= minPasswordLength, i <=maxPasswordLength; i++) {
+     var randomNumber = Math.floor(Math.random() * characters.length);
+    password += characters.substring(randomNumber, randomNumber +1);
+   } 
+     return document.getElementById("password").value = password;
+    
  }
 
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
+    passwordText.value = password;
+  }
+  
 generateBtn.addEventListener("click",writePassword);
